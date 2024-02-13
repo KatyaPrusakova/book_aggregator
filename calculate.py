85593ecff70e6b1bf16fd33c1829e1ced35cf713
KatyaPrusakova <katia.prusakova@gmail.com>
2024-02-13 17:33:11 +0100

    feat: limit_orders printed

--- calculate.py
+++ calculate.py
@@ -3,13 +3,19 @@ def calculate_best_price(order_books_combined, quantity, side):
     total_price = 0
     accumulated_quantity = 0
     included_orders = []  # List to keep track of orders used
+    limit_orders = {"coinbase": { "quantity":0, "price": 0}, 
+                   "gemini": { "quantity":0, "price": 0}, 
+                   "kraken": { "quantity":0, "price": 0}}
 
     # Sort orders: ascending for 'asks' to buy at lowest price, descending for 'bids' to sell at highest price
     orders = sorted(order_books_combined[side], key=lambda x: float(x['price']), reverse=(side == 'bids'))
-
     for order in orders:
         order_price = float(order['price'])
         order_quantity = float(order['amount'])
+    
+          # If we've accumulated enough to satisfy the requested quantity, exit the loop
+        if accumulated_quantity >= quantity:
+            break
 
         # Determine how much of this order can be used
         usable_quantity = min(order_quantity, quantity - accumulated_quantity)
@@ -21,13 +27,15 @@ def calculate_best_price(order_books_combined, quantity, side):
         included_order['amount'] = str(usable_quantity)  # Update the quantity to the amount actually used
         included_orders.append(included_order)
 
-        # If we've accumulated enough to satisfy the requested quantity, exit the loop
-        if accumulated_quantity >= quantity:
-            break
+        limit_orders[order["exchange"]]["quantity"] +=  order_quantity
+        limit_orders[order["exchange"]]["price"] = order['price']
 
     # Check if we've met the desired quantity
-    if accumulated_quantity < quantity:
+    if accumulated_quantity < quantity :
         # Not enough quantity available to fulfill the order
         return None, []
 
+    print("limit_orders:")
+    print(limit_orders)
+    print("\n")
     return total_price, included_orders
